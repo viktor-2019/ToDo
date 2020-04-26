e76010f214c2f8e01f6975d97c299392a481c7de
Viktor Dmytrenko <viktor.dmytrenko-19@gmail.com>
2020-04-26 11:00:54 +0300

    to do first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+let data = [
+    'Awake',
+    'To do my homework',
+    'To watch Yourtube',
+    'To learn JS',
+  ];
+
+  handlerTriggerButton = event => {
+    let offOn = event.currentTarget.querySelector('.button');
+    offOn.hidden = !offOn.hidden;
+  }
+
+handleDeletePanel = event => {
+  let target = event.target;
+  let del = target.closest('.panel').innerText;
+  let deleted = del.replace('[X]\n', '');
+  let deletedItem = deleted.slice(3);
+  //  It seems to me I will do this "if" as idiot :(
+  if (navigator.userAgent.includes('Safari') &&
+      !navigator.userAgent.includes('Mobile')) {
+    deletedItem = deleted.slice(6);
+  };
+  data = data.filter(el => el !== deletedItem);
+  target.closest('.panel').hidden = true;
+  list.innerHTML = '';
+  makeList(data);
+}
+
+handleAddItem = event => {
+  let inputItem = document.querySelector('#task').value;
+  if (!inputItem) return;
+  data.push(inputItem);
+  list.innerHTML = '';
+  makeList(data);
+  document.querySelector('#task').value = '';
+}
+
+let list = document.getElementById('list');
+let btnAdd = document.querySelector('.btnAdd');
+btnAdd.addEventListener('click', handleAddItem);
+
+function makeList(arr) {
+  let j = 0;
+  for (let i of arr) {
+    j++;
+    let pane = document.createElement("div");
+    pane.classList.add("panel");
+    pane.innerHTML = `<b>${j}. ${i}</b>`;
+    let btn = document.createElement("div");
+    btn.classList.add("button");
+    btn.innerHTML = `[X]`;
+    btn.setAttribute("hidden", true);
+    btn.addEventListener("click", handleDeletePanel);
+    pane.prepend(btn);
+    pane.addEventListener(
+      "mouseenter",
+      handlerTriggerButton
+    );
+    pane.addEventListener(
+      "mouseleave",
+      handlerTriggerButton
+    );
+    list.append(pane);
+  }
+}
+makeList(data);
